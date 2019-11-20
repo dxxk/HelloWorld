237aee6b267d8949cbed80d9cf5e101f7650cbee
dxxk <docpub@gmail.com>
2019-11-20 18:24:22 +0100

    info o Enter

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
         Scanner input = new Scanner(System.in);
         String end = null;
         do {
-            System.out.println("Wpisz '"+ EXIT + "', aby zakończyć program");
+            System.out.println("Aby zakończyć program wpisz '"+ EXIT + "' i naciśnij Enter");
             end = input.nextLine().toLowerCase();
         } while (!end.equals(EXIT));
         input.close();
