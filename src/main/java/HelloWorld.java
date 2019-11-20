3b7a864f35ab93ecf8eb550e3146ff307736d17e
dxxk <docpub@gmail.com>
2019-11-20 18:19:24 +0100

    dodane przywitanie

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class HelloWorld {
     private static final String EXIT = "exit";
     public static void main(String[] args) {
+        System.out.println("Witaj!");
         Scanner input = new Scanner(System.in);
         String end = null;
         do {
